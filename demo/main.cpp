042e93bc60dff2f3c72608e93ee8ae3de7c9c73f
SovesT1337 <temirkhan2002@gmail.com>
2022-01-07 03:17:49 +0300

    Commit I

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,82 @@
-#include <example.hpp>
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
 
-int main() {
-  example();
+//------------------------------------------------------------------------------
+//
+// Example: HTTP client, synchronous
+//
+//------------------------------------------------------------------------------
+
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <nlohmann/json.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+
+// Performs an HTTP GET and prints the response
+int main(int argc, char** argv)
+{
+  try
+  {
+    if(argc != 5)
+    {
+      std::cerr <<
+          "Usage: http-client-sync <host> <port> <target> <text>\n" <<
+          "Example:\n" <<
+          "    http-client-sync www.example.com 80 suggest \"any text\"\n";
+      return EXIT_FAILURE;
+    }
+    auto const host = argv[1];
+    auto const port = argv[2];
+    auto const target = argv[3];
+    std::string jstext = R"({"input": ")";
+        jstext += argv[4];
+        jstext += "\"}";
+
+    net::io_context ioc;
+    tcp::resolver resolver(ioc);
+    beast::tcp_stream stream(ioc);
+    auto const results = resolver.resolve(host, port);
+    stream.connect(results);
+
+    http::request<http::string_body> req{http::verb::post, target, 11};
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    req.set(http::field::content_type, "application/json");
+    req.body() = nlohmann::json::parse(jstext).dump();
+    req.prepare_payload();
+
+    http::write(stream, req);
+    beast::flat_buffer buffer;
+
+    http::response<http::dynamic_body> res;
+    http::read(stream, buffer, res);
+    std::cout << res << std::endl;
+    beast::error_code ec;
+    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    if(ec && ec != beast::errc::not_connected)
+      throw beast::system_error{ec};
+  }
+  catch(std::exception const& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
